7e528bef753ce486e92acafcbb05e5c9d5095ac2
Thomas Bender <thomasmatthewbender@gmail.com>
2022-03-03 14:56:33 -0500

    commit tree 372436ff542e81640d5b00bb2a5c88285e086c9a

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+// Write your code here
+
+const node = document.getElementById("main");
+if (node.parentNode) {
+  node.parentNode.removeChild(node);
+}
+
+const newHeader = document.createElement("h1");
+
+// document.body.append(newHeader);
+
+
+// newHeader.setAttribute("id", "victory")
+newHeader.id = "victory"
+newHeader.textContent = "YOUR-NAME is the champion"
+
+// instead of textContent can use innerText or innerHTML sometimes
